e517a3aba377e9cd3a70eb8bcffe9cfa9c818969
Jidoex <pierre.anxionnat@etu.univ-lorraine.fr>
2023-10-13 16:03:45 +0200

    Added movement to the player

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,26 +1,28 @@
 extends CharacterBody2D
 
-@export var speed: float
-@export var angular_speed: float
-@export var JUMP_VELOCITY = -400.0
-
+@export var speed: float = 10_000
+@export var angular_speed: float = 3
+@export var deceleration_factor: float = 10
 
 var screen_size
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
-
-	var next_position : Vector2
-	if Input.is_action_pressed("move_forward"):
-		next_position = Vector2(speed*delta, 0)
-	elif Input.is_action_pressed("move_backward"):
-		next_position = Vector2(-speed*delta, 0)
-	position = next_position
+func _physics_process(delta):
+	drive(delta)
+	flip(delta)
+	move_and_slide()
 	
-	var angle = Input.get_axis("rotate_anticlockwise","rotate_clockwise")
-	if angle:
-		rotation += angle * angular_speed
+func drive(delta):
+	var direction = Input.get_axis("move_backward","move_forward")
+	if direction:
+		velocity = Vector2(direction * speed * delta, 0)
+	elif velocity.x != 0:
+		velocity = Vector2(move_toward(velocity.x, 0, deceleration_factor), velocity.y)
+		
+func flip(delta):
+	var rotation_direction = Input.get_axis("rotate_anticlockwise","rotate_clockwise")
+	if rotation_direction:
+		rotate(rotation_direction * angular_speed * delta)
