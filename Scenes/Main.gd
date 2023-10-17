465012a05893de086e616ce736c8e45b2660664d
Jidoex <pierre.anxionnat@etu.univ-lorraine.fr>
2023-10-17 16:28:56 +0200

    Started working on the relation between coins and the player, but it doesn' work. I still don't fully get how signals work I guess..

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -2,3 +2,6 @@ extends Node
 
 var score : int = 0 # Number of chunks completed without dying
 var coins_collected : int = 0 # Number of coins collected during the game
+
+func coin_collected() -> void:
+	coins_collected += 1
---
+++ Scenes/Test.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var coins_collected : int = 0
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
+
+func _on_coin_collected():
+	coins_collected += 1
+	print("coin collected ! uwu")
